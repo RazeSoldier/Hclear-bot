b7945dd5cba5a0df310987012a5234c47ad61f05
RazeSoldier <razesoldier@outlook.com>
2018-04-15 13:34:59 +0800

    Update

--- includes/GlobalFunctions.php
+++ includes/GlobalFunctions.php
@@ -62,7 +62,7 @@ function findSubStr(string $str, string $find, int $count, int $offset = 0) : in
  * @param int $errline
  * @throws ErrorException
  */
-function exception_error_handler(int $errno, string $errstr,string $errfile, int $errline) {
+function errorHandler(int $errno, string $errstr,string $errfile, int $errline) {
     throw new \ErrorException( $errstr, 0, $errno, $errfile, $errline );
 }
 
